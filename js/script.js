811c0692742cb00a9ac6dc908c99e91b92bd9668
Adriano Romero <adriano.romerofco@gmail.com>
2022-10-03 15:58:45 -0300

    adicionando função desencriptar

--- js/script.js
+++ js/script.js
@@ -7,6 +7,12 @@ function btnEncriptar() {
     resultado.style.backgroundImage ="none";
 }
 
+function btnDesencritpar() {
+    const textoEncriptado = desencriptar(inputTexto.value);
+    resultado.value = textoEncriptado;
+    resultado.style.backgroundImage ="none";
+}
+
 function encriptar(stringEncriptada) {
     let matrizCodigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
 
@@ -15,7 +21,23 @@ function encriptar(stringEncriptada) {
 
     for(let i=0; i < matrizCodigo.length; i++) {
         if(stringEncriptada.includes(matrizCodigo[i][0])) {
-            stringDesencriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+
+    return stringDesencriptada;
+
+}
+
+function desencriptar(stringEncriptada) {
+    let matrizCodigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+
+    stringEncriptada = stringEncriptada.toLowerCase();
+    let stringDesencriptada = stringEncriptada.toLowerCase();
+
+    for(let i=0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
         }
     }
 
